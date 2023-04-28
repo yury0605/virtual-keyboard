40ffa2c278893ea81b24e57ee96bf4d37d16471c
yury0605 <122359936+yury0605@users.noreply.github.com>
2023-04-28 18:34:40 +0300

    feat: local storage saving done and keys initializing remade

--- index.js
+++ index.js
@@ -1,19 +1,96 @@
-const keyboard = [113, 119, 101, 114, 116, 121, 117, 105, 111, 112, 91, 93, 97, 115, 100, 102, 103, 104, 106, 107, 108, 59, 39, 92, 122, 120, 99, 118, 98, 110, 109, 44, 46, 47];
-/* document.onkeypress = function (event) {
-    keyboard.push(event.charCode);
-    console.log(keyboard) 
-}*/
+let wrapper = document.createElement('div');
+wrapper.classList.add('wrapper');
+document.body.append(wrapper);
 
-function init() {
-    let out = '';
-    for (let i = 0; i < keyboard.length; i++) {
-        out += '<div class="key" data="' + keyboard[i] + '" >' + String.fromCharCode(keyboard[i]) + '</div>';
+wrapper.insertAdjacentHTML('afterbegin', '<textarea class="input" autofocus></textarea>');
+wrapper.insertAdjacentHTML('beforeend', '<div class="keybord-area"></div>');
+wrapper.insertAdjacentHTML('beforeend', '<p class="instruction">Переключение языка ввода виртуальной клавиатуры происходит одновременным нажатием клавиш LeftShift и LeftCtrl на физической клавиатуре. Клавиатура была сделана в системе MacOs.</p>');
+
+let keybordArea = document.querySelector('.keybord-area');
+let inputArea = document.querySelector('.input');
+
+
+// массив английской раскладки символов lowercase 
+const englishLowercase = [
+    ['§', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '&#8592'],
+    ['&#8677', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']','&#8626'],
+    ['&#8682', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '\\'],
+    ['&#8679', '`', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '&#8679'],
+    ['ctrl', 'alt', 'cmd', ' ', 'cmd', 'alt', '&#9666', '&#9662', '&#9662', '&#9656']
+];
+
+const englishUppercase = [
+    '±', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '&#8592',
+    '&#8677', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}','&#8626',
+    '&#8682', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', '|',
+    '&#8679', '~', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', '&#8679',
+    'ctrl', 'alt', 'cmd', ' ', 'cmd', 'alt', '&#9666', '&#9662', '&#9662', '&#9656'
+];
+
+const russianLowercase = [
+    'ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '&#8592',
+    '&#8677', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '&#8626',
+    '&#8682', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', '\\',
+    '&#8679', ']', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '&#8679',
+    'ctrl', 'alt', 'cmd', ' ', 'cmd', 'alt', '&#9666', '&#9662', '&#9662', '&#9656'
+];
+
+const russianUppercase = [
+    'Ё', '!', '"', '№', ';', '%', ':', '?', '*', '(', ')', '_', '+', '&#8592',
+    '&#8677', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '&#8626',
+    '&#8682', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', '/',
+    '&#8679', '[', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', ',', '&#8679',
+    'ctrl', 'alt', 'cmd', ' ', 'cmd', 'alt', '&#9666', '&#9662', '&#9662', '&#9656'
+];
+
+
+// массив кодов физической клавиатуры
+const keyCodeList = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace',
+                      'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Enter',
+                      'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Backslash',
+                      'ShiftLeft', 'IntlBackslash', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ShiftRight',
+                      'ControlLeft', 'AltLeft', 'MetaLeft', 'Space', 'MetaRight', 'AltRight', 'ArrowLeft', 'ArrowUp', 'ArrowDown', 'ArrowRight'
+];
+
+
+// перебор элементов массива символов, генерация элементов DOM, перевод englishLowercase в одномерный массив
+
+let englishLowercaseList = [];
+
+for (let j = 0; j < englishLowercase.length; j++){
+    let keybordRow = document.createElement('div');
+    keybordRow.classList.add('keybord-row')
+    keybordArea.append(keybordRow);
+
+    for (let i=0; i < englishLowercase[j].length; i++){
+        let key = document.createElement('span');
+        key.classList.add('key');
+        keybordRow.append(key);
+        englishLowercaseList.push(englishLowercase[j][i])
     }
-    document.querySelector('#keyboard').innerHTML = out;
-}
-init();
 
-document.onkeypress = function (event) {
-    console.log(event.code);
-    document.querySelector('#keyboard .key[data="'+event.keyCode+'"]').classList.add('active');
+}
+
+// создание коллекции элементов DOM клавиши и объявление функциональных клавиш и присвоение стилей им:
+
+let keys = document.querySelectorAll('span');
+
+// функция заполнения клавиш символами
+
+function fillKeys (symbols){
+    for(let i = 0; i < keys.length; i++){
+        keys[i].innerHTML = symbols[i];
+    }
+}
+
+// сохранение раскладки в localStorage
+
+let lang;
+let languageStorage;
+let language = JSON.parse(localStorage.getItem(lang));
+
+if (!language){
+    fillKeys(englishLowercaseList)
+} else {
+    fillKeys(language);
 }
